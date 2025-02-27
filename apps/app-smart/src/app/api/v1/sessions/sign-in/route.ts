1e23e0219a011fd9539e12087c55eb107831dbc1
Ed <128392461+edtheanchor@users.noreply.github.com>
2025-02-27 10:46:07 +0000

    New commit: New commit message: snags to logon api handler

--- apps/app-smart/src/app/api/v1/sessions/sign-in/route.ts
+++ apps/app-smart/src/app/api/v1/sessions/sign-in/route.ts
@@ -1,17 +1,19 @@
 import { composeMiddleware } from 'lib/middleware';
 import { auth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const handler = async (): Promise<Response> => {
   const { userId } = await auth();
-  console.log('✅ sing-in auth handler triggered');
+  console.log('🤖 auth user logon action checks');
 
   if (!userId) {
     console.log('❌ sing-in auth handler userId:', userId);
-    return Response.redirect('/sign-in');
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
   }
-  console.log('✅ sing-in auth handler userId:', userId);
 
-  return Response.redirect('/dashboard');
+  return NextResponse.redirect(
+    new URL('/dashboard', process.env.NEXT_PUBLIC_APP_URL)
+  );
 };
 
 export const GET = composeMiddleware([handler]);
