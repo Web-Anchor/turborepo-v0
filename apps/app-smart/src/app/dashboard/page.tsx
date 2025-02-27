0eed0957913a9cf7da8ec8da104a1452927bb4cd
Ed <128392461+edtheanchor@users.noreply.github.com>
2025-02-27 16:02:51 +0000

    New commit: New commit message: dashboard filler snags

--- apps/app-smart/src/app/dashboard/page.tsx
+++ apps/app-smart/src/app/dashboard/page.tsx
@@ -7,16 +7,24 @@ export default async function Page() {
     <div className="flex flex-col gap-4">
       <PageTitle text="Dashboard" />
       <BentoGridWrapper className="lg:grid-cols-6 lg:grid-rows-2">
-        <Filler className="lg:col-span-2" />
-        <Filler className="lg:col-span-2" />
-        <Filler className="lg:col-span-2" />
+        <Filler className="lg:col-span-2">OverviewCard/Redirect</Filler>
+        <Filler className="lg:col-span-2">OverviewCard/Redirect</Filler>
+        <Filler className="lg:col-span-2">OverviewCard/Redirect</Filler>
         <Filler className="lg:col-span-3" />
         <Filler className="lg:col-span-3" />
-        <Filler className="lg:col-span-4" />
-        <Filler className="lg:col-span-2" />
+        <Filler className="lg:col-span-4 h-72">
+          List of recent updates (new orders, stock adjustments)
+        </Filler>
+        <Filler className="lg:col-span-2">
+          Metrics for low-stock alerts, recent orders, sales totals
+        </Filler>
 
-        <Filler className="lg:col-span-3" />
-        <Filler className="lg:col-span-3" />
+        <Filler className="lg:col-span-3 h-56">
+          Mini chart components (line/bar charts) for quick trends
+        </Filler>
+        <Filler className="lg:col-span-3">
+          Mini chart components (line/bar charts) for quick trends
+        </Filler>
       </BentoGridWrapper>
       <Paragraph text="Sunt magna elit cillum aliqua exercitation labore et adipisicing ullamco in." />
     </div>
--- packages/ui/src/filler.tsx
+++ packages/ui/src/filler.tsx
@@ -8,12 +8,12 @@ export function Filler(props: ComponentTypes) {
   return (
     <div
       className={classNames(
-        'overflow-hidden bg-gray-50 sm:rounded-lg text-slate-800',
+        'flex justify-center align-center overflow-hidden bg-gray-50 sm:rounded-lg text-slate-800',
         props.className
       )}
     >
-      <div className="px-4 py-5 sm:p-6 flex justify-center items-center">
-        {props.children || 'Filler content'}
+      <div className="px-4 py-5 sm:p-6 flex flex-col justify-center align-center">
+        {props.children || <p>Filler content</p>}
       </div>
     </div>
   );
