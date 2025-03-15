bf073a9c5b054add4e7ce83e2c34cb6262962ddd
Ed <128392461+edtheanchor@users.noreply.github.com>
2025-03-15 17:16:56 +0000

    New commit: New commit message: implement prev card and page ui refactoring

--- apps/app-smart/src/app/dashboard/inventory/[id]/page.tsx
+++ apps/app-smart/src/app/dashboard/inventory/[id]/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { PageTitle, Paragraph } from '@repo/ui/documents';
 import Link from 'components/Wrappers/Link';
 import { Button } from '@repo/ui/buttons';
 import { toast } from 'sonner';
@@ -9,10 +8,19 @@ import { useParams, useRouter } from 'next/navigation';
 import { FormWrapper, TextInput } from '@repo/ui/forms';
 import { useGetInventory } from 'hooks/inventories';
 import { objKeysToNumber } from 'lib/utils';
+import { PageWrapper } from '@repo/ui/semantic';
+import { DetailCard } from '@repo/ui/cards/DetailCard';
+import { Header } from '@repo/ui/headings/header';
+import { useState } from 'react';
+
+type ComponentState = {
+  fetching?: boolean;
+};
 
 export default function Page() {
   const router = useRouter();
   const params = useParams<{ id: string }>();
+  const [state, setState] = useState<ComponentState>({});
   const { data, mutate } = useGetInventory({ id: params?.id });
 
   async function submit(data: { [k: string]: FormDataEntryValue }) {
@@ -20,6 +28,7 @@ export default function Page() {
       if (!params?.id) {
         throw new Error('Inventory ID is required');
       }
+      setState((s) => ({ ...s, fetching: true }));
       await axios.post('/api/v1/inventories/update', {
         ...objKeysToNumber(['quantity', 'price'], data),
         id: params?.id,
@@ -31,102 +40,115 @@ export default function Page() {
       toast.error(
         (error as Error)?.message || 'An error occurred. Please try again.'
       );
+    } finally {
+      setState((s) => ({ ...s, fetching: false }));
     }
   }
 
   return (
-    <section className="max-w-3xl mx-auto px-6 py-12 space-y-12">
-      <PageTitle>Inventory ID: {params?.id}</PageTitle>
+    <PageWrapper>
+      <Header title={data?.name || 'Inventory Item'} type="page-header" />
       <FormWrapper onSubmit={submit}>
-        <div className="space-y-12">
-          <div className="border-b border-white/10 pb-12">
-            <h2 className="text-base/7 font-semibold text-white">Cluster</h2>
-            <p className="mt-1 text-sm/6 text-gray-400">
-              Do Lorem labore amet commodo elit non incididunt nostrud eiusmod
-              laborum labore.
-            </p>
-
-            <div className="mt-10 flex flex-col gap-8">
-              <TextInput
-                name="name"
-                label="Name"
-                placeholder="Enter a name"
-                defaultValue={data?.name}
-              />
-              <TextInput
-                name="description"
-                label="Description"
-                placeholder="Enter a description"
-                defaultValue={data?.description}
-                optional
-              />
-              <TextInput
-                name="sku"
-                label="SKU"
-                placeholder="Enter a SKU"
-                defaultValue={data?.sku}
-                optional
-              />
-              <TextInput
-                name="unit"
-                label="Unit"
-                placeholder="Enter a unit"
-                defaultValue={data?.unit}
-                optional
-              />
-              <TextInput
-                name="quantity"
-                type="number"
-                label="Quantity"
-                placeholder="Enter a quantity"
-                defaultValue={data?.quantity?.toString()}
-                optional
-              />
-              <TextInput
-                name="supplier"
-                label="Supplier"
-                placeholder="Enter a supplier"
-                defaultValue={data?.supplier}
-                optional
-              />
+        <DetailCard
+          title={data?.name || 'Inventory Item'}
+          description={data?.description || 'Description of the inventory item'}
+          list={[
+            {
+              name: 'Name',
+              value: (
+                <TextInput
+                  name="name"
+                  placeholder="Enter a name"
+                  defaultValue={data?.name}
+                />
+              ),
+            },
+            {
+              name: 'Description',
+              value: (
+                <TextInput
+                  name="description"
+                  placeholder="Enter a description"
+                  defaultValue={data?.description}
+                  optional
+                />
+              ),
+            },
+            {
+              name: 'SKU',
+              value: (
+                <TextInput
+                  name="sku"
+                  placeholder="Enter a SKU"
+                  defaultValue={data?.sku}
+                  optional
+                />
+              ),
+            },
+            {
+              name: 'Unit',
+              value: (
+                <TextInput
+                  name="unit"
+                  placeholder="Enter a unit"
+                  defaultValue={data?.unit}
+                  optional
+                />
+              ),
+            },
+            {
+              name: 'Quantity',
+              value: (
+                <TextInput
+                  name="quantity"
+                  type="number"
+                  placeholder="Enter a quantity"
+                  defaultValue={data?.quantity?.toString()}
+                  optional
+                />
+              ),
+            },
+            {
+              name: 'Supplier',
+              value: (
+                <TextInput
+                  name="supplier"
+                  placeholder="Enter a supplier"
+                  defaultValue={data?.supplier}
+                  optional
+                />
+              ),
+            },
+          ]}
+          footer={
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+              <Button
+                type="button"
+                variant="link"
+                className="text-sm/6 font-semibold text-white"
+                LinkComponent={Link}
+                href="/dashboard/lists"
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                variant="primary"
+                isLoading={state.fetching}
+              >
+                Update
+              </Button>
             </div>
-          </div>
-        </div>
-
-        <div className="mt-6 flex items-center justify-end gap-x-6">
-          <Button
-            type="button"
-            variant="link"
-            className="text-sm/6 font-semibold text-white"
-            LinkComponent={Link}
-            href="/dashboard/lists"
-          >
-            Cancel
-          </Button>
-          <Button type="submit" variant="primary">
-            Update
-          </Button>
-        </div>
+          }
+        />
       </FormWrapper>
-      <Paragraph>
-        *Exercitation ullamco est fugiat aliqua voluptate velit nostrud
-        cupidatat consequat sunt veniam excepteur anim nisi. Do nostrud eu
-        consequat et sit ut enim in anim quis exercitation officia. Adipisicing
-        sint minim ex id ex. Excepteur duis consectetur ad veniam exercitation
-        fugiat elit mollit. Nostrud consectetur deserunt esse quis dolore amet
-        fugiat sint veniam nostrud voluptate non. Lorem et dolore sit voluptate.
-        Aute irure aliquip do adipisicing laborum irure deserunt Lorem
-        consectetur id. Velit aute ea sint commodo ea qui Lorem. Cupidatat
-        cupidatat aute excepteur anim quis eu ex voluptate adipisicing cillum.
-        Esse culpa culpa amet dolor reprehenderit dolore consequat. Aliquip
-        nostrud do exercitation enim non amet in. Et aliqua dolore voluptate
-        elit proident. Deserunt magna fugiat ea nostrud qui culpa ullamco est
-        nisi Lorem sit. Ipsum est sit magna incididunt esse elit in sunt ex
-        veniam ullamco exercitation exercitation duis. Aliquip reprehenderit
-        excepteur qui ex deserunt cupidatat magna do velit nostrud elit non
-        veniam nisi. Non id qui sunt nulla tempor occaecat ut nostrud Lorem.
-        Labore quis anim officia do sint fugiat irure.
-      </Paragraph>
-    </section>
+      <Header
+        subtitle="Aliquip aliquip non veniam sit reprehenderit cillum proident."
+        description={[
+          'Anim occaecat adipisicing ipsum magna quis. Amet et id voluptate occaecat dolor adipisicing aute commodo ex commodo. Pariatur nisi ad pariatur minim. Commodo consequat velit labore in nulla ullamco ipsum. Dolore anim adipisicing incididunt irure aute esse irure ullamco amet laboris aliquip. Ea anim anim sunt veniam ipsum tempor proident eu duis proident magna. Ea dolor id Lorem amet amet laboris aliquip magna. Eu ea ut consequat magna sunt enim ad nulla qui labore ea. Occaecat ullamco reprehenderit tempor est velit aliqua sit sint et nisi sit. Esse anim ad id commodo ea esse cillum laboris magna aliqua proident aute incididunt. Ullamco tempor Lorem id consectetur aliqua. Deserunt do enim officia occaecat occaecat eiusmod commodo nisi minim cupidatat sint mollit do reprehenderit. Ea est occaecat do amet culpa eu irure est enim excepteur duis ipsum.',
+        ]}
+        type="page-header"
+      />
+    </PageWrapper>
   );
 }
---
+++ packages/ui/src/cards/DetailCard.tsx
@@ -0,0 +1,68 @@
+import { classNames } from '../../dist/utils';
+
+export type ListType = {
+  name: string;
+  value?: string | React.ReactNode;
+};
+
+type DetailCardProps = {
+  title: string;
+  description: string;
+  list: ListType[];
+  className?: string;
+  footer?: React.ReactNode;
+};
+
+export function DetailCard({
+  title,
+  description,
+  list,
+  className,
+  footer,
+}: DetailCardProps) {
+  return (
+    <div
+      className={classNames(
+        'overflow-hidden bg-slate-800 sm:rounded-lg shadow-lg',
+        className
+      )}
+    >
+      {(title || description) && (
+        <div className="px-4 py-6 sm:px-6">
+          {title && (
+            <h3 className="text-base/7 font-semibold light:text-gray-900">
+              {title}
+            </h3>
+          )}
+          {description && (
+            <p className="mt-1 max-w-2xl text-sm/6 light:text-gray-500">
+              {description}
+            </p>
+          )}
+        </div>
+      )}
+      <div className="border-t border-white/10">
+        <dl className="divide-y divide-white/10">
+          {list.map((item, index) => (
+            <div
+              key={index}
+              className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+            >
+              <dt className="text-sm font-medium light:text-gray-900">
+                {item.name}
+              </dt>
+              <dd className="mt-1 text-sm/6 light:text-gray-700 sm:col-span-2 sm:mt-0">
+                {item.value}
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+      {footer && (
+        <div className="px-4 py-6 sm:px-6 border-t border-white/10">
+          {footer}
+        </div>
+      )}
+    </div>
+  );
+}
