ad926ca4494138b68b456fae90b44e84b13d6bc9
Ed <128392461+edtheanchor@users.noreply.github.com>
2025-03-21 16:58:26 +0000

    New commit: New commit message: img import deprecations changes

--- apps/smart-app/next.config.ts
+++ apps/smart-app/next.config.ts
@@ -3,7 +3,17 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['picsum.photos', 'img.clerk.com', 'tailwindcss.com'],
+    remotePatterns: [
+      {
+        hostname: 'picsum.photos',
+      },
+      {
+        hostname: 'img.clerk.com',
+      },
+      {
+        hostname: 'tailwindcss.com',
+      },
+    ],
   },
 };
 
